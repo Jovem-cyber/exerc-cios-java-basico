a96ff2ad7a7efbccd194c58e733b5efe3c08890a
Guilherme Monteiro <gnoborghini@gmail.com>
2025-06-25 21:01:50 -0300

    POO - Desafio
    Modelagem e Diagramação de um Componente iPhone em Java.

---
+++ Desafio JAVA - Modelagem e Diagramação de um Componente iPhone/main/src/AparelhoTelefonico.java
@@ -0,0 +1,17 @@
+public class AparelhoTelefonico implements {
+
+    public void ligar() {
+
+        System.out.println("Chamando Fulano.");
+    }
+
+    public void atender(){
+
+        System.out.println("Fulano está ligando...");
+    }
+
+    public void iniciarCorreioVoz() {
+
+        System.out.println("Iniciar correio de voz.");
+    }
+}
